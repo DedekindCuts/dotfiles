7bfbaef1a15feffd65be2ba9661c21f68f98c807
Nick Larsen <n.larsen@veracityfa.com>
2019-03-26 10:38:32 -0600

    Add 'profiles' to specify different desired configurations

---
+++ .brew/brew-install.sh
@@ -0,0 +1,157 @@
+#!/bin/bash
+
+# check if Homebrew is installed and install if not
+if which brew >/dev/null; then
+	# echo "$(tput setaf 2)Homebrew already installed.$(tput setaf 7)"
+	# echo "Updating Homebrew..."
+	brew update >/dev/null
+	echo "$(tput setaf 2)Homebrew installed and up to date.$(tput setaf 7)"
+else
+	echo "Installing Homebrew..."
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	echo "$(tput setaf 2)Installed.$(tput setaf 7)"
+fi
+
+# check if Git (Homebrew) is installed and install if not
+if brew ls --versions git >/dev/null; then
+	if brew outdated | grep git >/dev/null; then
+		echo "Updating Git..."
+		brew upgrade git >/dev/null
+	else
+		:
+	fi
+	echo "$(tput setaf 2)Git installed and up to date.$(tput setaf 7)"
+else
+	echo "Installing Git..."
+	brew install git
+	echo "$(tput setaf 2)Installed.$(tput setaf 7)"
+fi	
+
+# sign in to App Store
+echo "Signing in to App Store..."
+
+# Workaround for mas signin bug
+until (mas account > /dev/null); do 
+	echo "$(tput setaf 3)Please sign in using the App Store app.$(tput setaf 7)" 
+	open -a "/Applications/App Store.app"
+	echo "$(tput setaf 3)Waiting for signin...$(tput setaf 7)"
+	until (mas account > /dev/null); do 
+		sleep 3
+	done
+done
+##############################
+
+# Normal signin (uncomment if/when mas bug is resolved)
+# until (mas account > /dev/null); do
+#	mas signin $APPLE_ID &>/dev/null # will need to set APPLE_ID if this ever works
+# done
+
+if (mas account > /dev/null); then
+	echo "$(tput setaf 2)Signed in.$(tput setaf 7)"
+fi
+
+# prompt for type of install
+echo "What would you like to do?"
+select answer in "Quick update currently installed programs" \
+	"Install all programs from Brewfile" \
+	"Selectively update or install programs";
+do
+	case $answer in
+		"Quick update currently installed programs" )
+			brew upgrade
+			brew cask upgrade
+			mas upgrade
+			break;;
+		"Install all programs" )
+			brew bundle --file=$HOME/.brew/.Brewfile
+			break;;
+		"Selectively update or install programs" )
+			(brew bundle list --taps --file=$HOME/.brew/.Brewfile | while read -r tap ; do
+				echo "Tap $tap?"
+				select answer in "Yes" "No"
+				do
+					case $answer in
+						Yes )
+							brew tap "$tap"
+							break;;
+						No )
+							break;;
+					esac
+				done <&4
+			done) 4<&0
+			(brew bundle list --brews --file=$HOME/.brew/.Brewfile | while read -r brew ; do
+				echo "Install/upgrade $brew?"
+				select answer in "Yes" "No"
+				do
+					case $answer in
+						Yes )
+							if brew ls --versions "$brew" > /dev/null; then
+								echo "Already installed; updating."
+							else
+								brew install "$brew"
+							fi
+							break;;
+						No )
+							break;;
+					esac
+				done <&4
+			done) 4<&0
+			(brew bundle list --casks --file=$HOME/.brew/.Brewfile | while read -r cask ; do
+				echo "Install/upgrade $cask?"
+				select answer in "Yes" "No"
+				do
+					case $answer in
+						Yes )
+							if brew cask ls --versions "$cask" > /dev/null; then
+								echo "Already installed; updating."
+							else
+								brew cask install "$cask"
+							fi
+							break;;
+						No )
+							break;;
+					esac
+				done <&4
+			done) 4<&0
+			# silently install GNU grep if not already installed (GNU grep is required for extracting the mas ids and names from the mas_list file)
+			(if brew ls --versions grep > /dev/null; then :; else brew install grep &> /dev/null; fi
+			cat $HOME/.brew/.mas-list | while read -r mas ; do
+				id="$(echo $mas | ggrep -oP '^\d+')"
+				name="$(echo $mas | ggrep -oP '(?<=\h)[\w\h\d-:]*')"
+				echo "Install/upgrade $name?"
+				select answer in "Yes" "No"
+				do
+					case $answer in
+						Yes )
+							if mas list | grep "$id" &> /dev/null; then 
+								echo "Already installed; updating."
+							else 
+								mas install "$id"
+							fi
+							break;;
+						No )
+							break;;
+					esac
+				done <&4
+			done) 4<&0
+			# upgrade all installed brews and casks (doing them individually gives an error for brews and uninstalls and reinstalls for casks)
+			echo "Updating..."
+			brew upgrade
+			brew cask upgrade
+			mas upgrade
+			break;;
+	esac
+done
+
+# prompt for cleanup
+echo "Would you like to clean up by removing old versions of programs?"
+select answer in "Yes" "No"
+do
+	case $answer in
+		Yes )
+			brew cleanup
+			break;;
+		No )
+			break;;
+	esac
+done
---
+++ .vscode-settings/vscode-setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# prompt to install VS code if it is not found
+while [[ ! -d "$HOME/Library/Application Support/Code" ]]; do
+	echo "Before continuing, VS Code must be installed."
+	echo "(https://code.visualstudio.com/download)"
+	read -n1 -rsp $'Press any key to continue once VS Code is installed, or CTRL+C to exit.\n'
+done
+
+# add VS Code to PATH to allow launching from the terminal (using `code`)
+# (if it's installed and not already in PATH)
+if [[ -e "/Applications/Visual Studio Code.app" ]]; then
+	if echo $PATH | grep -q "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"; then
+		:
+	else
+		PATH=$PATH:"/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+	fi
+else
+	echo "Couldn't find '/Applications/Visual Studio Code.app'; check that is installed correctly and try again"
+	exit
+fi
+
+## apply custom VS Code settings
+ln -sfF "$HOME/.vscode-settings/snippets" "$HOME/Library/Application Support/Code/User"
+ln -sfF "$HOME/.vscode-settings/keybindings.json" "$HOME/Library/Application Support/Code/User"
+ln -sfF "$HOME/.vscode-settings/settings.json" "$HOME/Library/Application Support/Code/User"
+
+## install VS Code extensions
+cat "$HOME/.vscode-settings/extensions-list.txt" | xargs -L 1 code --install-extension
